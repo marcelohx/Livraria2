b0467e7e8443bc65a84bc5a81435e4eefcf98102
MARCELO HENRIQUE XAVIER <marcelo.hxavier@senacsp.edu.br>
2024-05-02 22:03:58 -0300

    Sexto Commit

--- ConsoleApp1/DAOCompra.cs
+++ ConsoleApp1/DAOCompra.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -57,5 +58,74 @@ namespace ConsoleApp1
                 Console.WriteLine("Algo deu errado!\n\n" + erro);
             }
         }//Fim do metodo
-    }
-}
+        public void PreencherVetor()
+        {
+            string query = "select * from compra";
+            codigoCompra = new long[100];
+            CPF = new long[100];
+            nome = new string[150];
+            precoUnitario = new double[100];
+            precoTotal = new double[100];
+            quantidadeCompra = new int[100];
+            for(i = 0; i < 100; i++)
+            {
+                codigoCompra[i] = 0;
+                CPF[i] = 0;
+                nome[i] = "";
+                precoUnitario[i] = 0;
+                precoTotal[i] = 0;
+                quantidadeCompra[i] = 0;
+            }
+            MySqlCommand coletar = new MySqlCommand(query, conexao);
+            //Leitura do banco de dados
+            MySqlDataReader leitura = coletar.ExecuteReader();
+            i = 0;
+            while (leitura.Read())
+            {
+                codigoCompra[i] = Convert.ToInt64(leitura["codigoCompra"]);
+                CPF[i] = Convert.ToInt64(leitura["CPF"] + "");
+                nome[i] = leitura["nome"] + "";
+                precoUnitario[i] = Convert.ToDouble(leitura["precoUnitario"] + "");
+                precoTotal[i] = Convert.ToDouble(leitura["precoTotal"] + "");
+                quantidadeCompra[i] = Convert.ToInt32(leitura["quantidadeCompra"] + "");
+                i++;
+                contador++;
+            }//fim do while
+            leitura.Close();//Fecha a conexao com o banco
+        }//Fim do metodo
+        public string ConsultarTudo()
+        {
+            PreencherVetor();//Preencher vetores
+            msg = "";
+            for (i = 0; i < contador; i++)
+            {
+                msg += "\ncodigo da compra: " + codigoCompra[i] +
+                       ", CPF: " + CPF[i] +
+                       ", nome: " + nome[i] +
+                       ", preco unitario: " + precoUnitario[i] +
+                       ", precoTotal: " + precoTotal[i] +
+                       ", quantidadeCompra: " + quantidadeCompra[i];
+            }//Fim do for
+            return msg;
+        }//fim do metodo
+        public string ConsultarIndividual(long codCompra)
+        {
+            PreencherVetor();
+            msg = "";
+            for (i = 0; i < contador; i++)
+            {
+                if (codigoCompra[i] == codCompra)
+                {
+                    msg += "\ncodigo da compra: " + codigoCompra[i] +
+                      ", CPF: " + CPF[i] +
+                      ", nome: " + nome[i] +
+                      ", preco unitario: " + precoUnitario[i] +
+                      ", precoTotal: " + precoTotal[i] +
+                      ", quantidadeCompra: " + quantidadeCompra[i];
+                    return msg;
+                }//fim do if                
+            }//fim do for
+            return "Código não encontrado!";
+        }//Fim do consultar indiv.
+    }//Fim da classe
+}//Fim do projeto
